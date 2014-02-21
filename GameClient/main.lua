edca443c26dd0393362577bcb1d704c5fd583cc5
lucasgolino <lucasgolino@hotmail.com>
2014-02-21 03:00:11 -0300

    Melhorando Segurança com md5 e outros

--- GameClient/main.lua
+++ GameClient/main.lua
@@ -1,4 +1,5 @@
 
+local md5 = require "md5"
 local socket = require "socket"
 
 -- the address and port of the server
@@ -28,6 +29,8 @@ function love.load()
 	start_send = string.format("UserID(%s) Connecting", identy)
 
 	server.udp:send(start_send)
+
+	file_md5 = md5.file("GameClient.love")
 end
 	
 function love.draw()
@@ -36,13 +39,14 @@ function love.draw()
 	love.graphics.print("Update: "..update, 10, 90, 0, 2, 2)
 	love.graphics.print("Time Start: "..all_update, 10, 130, 0, 2, 2)
 	love.graphics.print("Ip: "..socket.dns.toip(socket.dns.gethostname()), 10, 170, 0, 2, 2)
+	love.graphics.print("Teste: "..file_md5, 10, 210, 0, 2, 2)
 end
 
 function love.update(dt)
 	if in_time >= time_send then 
 		if send == 1 then
 
-			SendPacket = string.format("%s", tostring(math.random(9999)).." UserID("..identy..")")
+			SendPacket = string.format("%s", tostring(math.random(9999)).." UserID("..identy..") "..file_md5)
 			server.udp:send(SendPacket)
 		end
 
@@ -76,56 +80,4 @@ end
 function love.quit()
 end
 
--- Lua 5.1+ base64 v3.0 (c) 2009 by Alex Kloss <alexthkloss@web.de>
--- licensed under the terms of the LGPL2
-
--- Lua 5.1+ base64 v3.0 (c) 2009 by Alex Kloss <alexthkloss@web.de>
--- licensed under the terms of the LGPL2
-
--- character table string
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/.,'
-
--- encoding
-function enc(data)
-    return ((data:gsub('.', function(x) 
-        local r,b='',x:byte()
-        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-        if (#x < 6) then return '' end
-        local c=0
-        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-        return b:sub(c+1,c+1)
-    end)..({ '', '==', '=' })[#data%3+1])
-end
-
--- decoding
-function dec(data)
-    data = string.gsub(data, '[^'..b..'=]', '')
-    return (data:gsub('.', function(x)
-        if (x == '=') then return '' end
-        local r,f='',(b:find(x)-1)
-        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
-        return r;
-    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
-        if (#x ~= 8) then return '' end
-        local c=0
-        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
-        return string.char(c)
-    end))
-end
-
--- command line if not called as library
-if (arg ~= nil) then
-	local func = 'enc'
-	for n,v in ipairs(arg) do
-		if (n > 0) then
-			if (v == "-h") then print "base64.lua [-e] [-d] text/data" break
-			elseif (v == "-e") then func = 'enc'
-			elseif (v == "-d") then func = 'dec'
-			else print(_G[func](v)) end
-		end
-	end
-else
-	module('base64',package.seeall)
-end
+require("base64")
--- GameServer/main.py
+++ GameServer/main.py
@@ -4,6 +4,9 @@ import re
 UDP_IP = "localhost"
 UDP_PORT = 9980
 
+
+ClientMd5 = "170f2b9ae1c722003930a27166276c47"
+
 sock = socket.socket(socket.AF_INET, # Internet
                  socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
@@ -16,9 +19,8 @@ print "Sarting UDP Server in (",UDP_IP,",",UDP_PORT,")"
 while True:
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 	m = p.split(data) #print "Packet recive:", p.match(data), addr
-	print m
 
-	if m[0].isdigit():
+	if m[0].isdigit() and m[3] == ClientMd5:
 		if type(int(m[0])) == int:
 			print  "Packet recive:", m[0], addr, m[1],"(",m[2],")"
 	else:
